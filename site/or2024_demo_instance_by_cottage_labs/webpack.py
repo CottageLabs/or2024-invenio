fa0b0f7aa8fe4bd33c085deb4b006c065b7c2394
cloo <cloo@or2024-invenio>
2024-05-31 09:17:15 +0000

    initial commit of vanilla inveniordm

---
+++ site/or2024_demo_instance_by_cottage_labs/webpack.py
@@ -0,0 +1,16 @@
+"""JS/CSS Webpack bundles for OR2024 Demo Instance by Cottage Labs."""
+
+from invenio_assets.webpack import WebpackThemeBundle
+
+theme = WebpackThemeBundle(
+    __name__,
+    "assets",
+    default="semantic-ui",
+    themes={
+        "semantic-ui": dict(
+            entry={
+                # Add your webpack entrypoints
+            },
+        ),
+    },
+)
